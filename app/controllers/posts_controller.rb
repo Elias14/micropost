30268068436d196b6b84856fb60047bddb756a3b
Elias Gutierrez <egutierrezd@uninorte.edu.co>
2014-04-26 20:45:29 -0400

    Personalize flash message abd Fix Edit Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_filter :get_users
   # GET /posts
   # GET /posts.json
   def index
@@ -25,8 +26,7 @@ class PostsController < ApplicationController
   # GET /posts/new.json
   def new
     @post = Post.new
-    @users = User.all.map {|user| [user.name, user.id]}
-
+    
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @post }
@@ -81,4 +81,8 @@ class PostsController < ApplicationController
       format.json { head :no_content }
     end
   end
+  private 
+  def get_users
+    @users = User.all.map {|user| [user.name, user.id]}
+end
 end
